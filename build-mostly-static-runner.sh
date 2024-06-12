35171e9b5850d0160d3f121b96136970c771a81f
Olya Gupalo <olga.gupalo@oracle.com>
2024-06-12 12:41:43 +0200

    Add GitHub Workflow

--- build-mostly-static-runner.sh
+++ build-mostly-static-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-native-image -Ob --static-nolibc -m jdk.httpserver -o runner.mostly-static
+native-image -Ob -H:+UnlockExperimentalVMOptions -H:+StaticExecutableWithDynamicLibC -m jdk.httpserver -o runner.mostly-static
 
 # Distroless Base (provides glibc)
 docker build . -f Dockerfile.distroless-base.mostly -t website-runner:distroless-base.mostly-static
